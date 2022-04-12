71914acbe2d6185d3d063c79476987f2574ce626
vini-kenzo <vkf5@hotmail.com>
2022-04-12 16:00:28 -0300

    histórico com datas

--- SistemaGerenciamentoOnibus/src/main/java/com/sistema/model/dao/CorridaDAO.java
+++ SistemaGerenciamentoOnibus/src/main/java/com/sistema/model/dao/CorridaDAO.java
@@ -5,6 +5,13 @@
 package com.sistema.model.dao;
 
 import com.sistema.model.pojo.Corrida;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import org.hibernate.HibernateException;
 
 /**
  *
@@ -15,5 +22,30 @@ public class CorridaDAO extends GenericDAO<Corrida, Integer>{
     public CorridaDAO() {
         super(Corrida.class);
     }
+    
+    public List<Corrida> list(Date dataInicial, Date dataFinal) {
+        EntityManager entityManager = super.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        List<Corrida> listaRetorno = new ArrayList<>();
+        try {
+            System.out.println(entityManager.toString());
+            transaction.begin();
+            if(dataInicial == null) dataInicial = new Date(0L);
+            if(dataFinal == null) dataFinal = new Date(System.currentTimeMillis());
+            Query query = entityManager.createQuery("SELECT c FROM Corrida c WHERE c.inicioCorrida > :dataInicial AND  c.fimCorrida < :dataFinal");
+            query.setParameter("dataInicial", dataInicial);
+            query.setParameter("dataFinal", dataFinal);
+            listaRetorno = query.getResultList();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
 
+        return listaRetorno;
+    }
 }
